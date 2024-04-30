022bfea517b1734d85bc99d9ef7ff78dca1b7a08
rerevana <revanafalihas@gmail.com>
2024-04-30 07:15:29 +0700

    tambah komentar

--- FlappyBird/src/App.java
+++ FlappyBird/src/App.java
@@ -1,3 +1,6 @@
+// Saya Revana Faliha Salma NIM 2202869 mengerjakan Soal Latihan Praktikum 7 dalam mata kuliah DPBO 
+//untuk keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
 import javax.swing.*;
 
 public class App {
